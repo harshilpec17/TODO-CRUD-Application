66c4a0434b76f1ab9f3e688fa0356b5cfa4b9d9b
harshilpec17 <harshilsuthar@hotmail.com>
2023-10-03 14:07:15 -0400

    Add todo method updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [todos, setTodos] = useState([]);
 
   const addTodo = (todo) => {
-    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+    setTodos((prev) => [{ ...todo }, ...prev]);
   };
 
   const updatedTodo = (id, todo) => {
